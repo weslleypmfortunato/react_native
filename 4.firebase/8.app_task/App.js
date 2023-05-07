2cc0a7424ea79e944d87290bf920aa39eafa6af1
weslleypmfortunato <weslley.m.fortunato@gmail.com>
2023-05-07 00:28:58 -0700

    app todo done

--- 4.firebase/8.app_task/App.js
+++ 4.firebase/8.app_task/App.js
@@ -1,9 +1,99 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, FlatList, Keyboard } from "react-native";
 import Login from "./src/components/Login";
+import TaskList from "./src/components/Login/TaskList";
+import firebase from './src/services/firebaseConnection'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons/faCircleXmark'
 
 export default function App () {
   const [user, setUser] = useState(null)
+  const [newTask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [key, setKey] = useState('')
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+
+    function getUser () {
+      if (!user) {
+        return
+      }
+      
+      firebase.database().ref('tarefas').child(user).once('value', (snapshot) => {
+        setTasks([])
+
+        snapshot?.forEach( (childItem) => {
+          let data = {
+            key: childItem.key,
+            nome: childItem.val().nome
+          }
+          setTasks(oldTasks => [...oldTasks, data])
+        })
+      })
+    }
+    getUser()
+  }, [user])
+
+  function handleAdd () {
+    if (newTask === '') {
+      return
+    }
+
+    if (key !== '') {
+      firebase.database().ref('tarefas').child(user).child(key).update({
+        nome: newTask
+      })
+        .then( () => {
+          const taskIndex = tasks.findIndex( item => item.key === key)
+          const taskClone = tasks
+          taskClone[taskIndex].nome = newTask
+
+          setTasks([...taskClone])
+        })
+        Keyboard.dismiss()
+        setNewTask('')
+        setKey('')
+        return // não esquecer deste return pois se não ele vai add uma nova atarefa ao inves de editar
+    }
+
+
+    let tarefas = firebase.database().ref('tarefas').child(user)
+    let chave = tarefas.push().key
+    tarefas.child(chave).set({
+      nome: newTask
+    })
+      .then( () => {
+        const data = {
+          key: chave,
+          nome: newTask
+        }
+        setTasks(oldTasks => [...oldTasks, data])
+      })
+      Keyboard.dismiss()
+      setNewTask('')
+  }
+
+  function handleDelete (key) {
+    firebase.database().ref('tarefas').child(user).child(key).remove()
+      .then( () => {
+        const findTasks = tasks.filter( item => item.key !== key)
+        setTasks(findTasks)
+      })
+  }
+
+  function handleEdit (data) {
+    setKey(data.key)
+    setNewTask(data.nome)
+    inputRef.current.focus() // aqui estou fazendo o keyboard levantar qdo pressiono para editar
+  }
+
+  function cancelEdit () {
+    setKey('')
+    setNewTask('')
+    Keyboard.dismiss()
+  }
 
   if (!user) {
     return <Login changeStatus={ (user) => setUser(user) }/>
@@ -11,7 +101,35 @@ export default function App () {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>LOGADO</Text>
+
+      { key.length > 0 && (
+        <View style={{ flexDirection: 'row', marginBottom: 2, marginTop: 8, alignItems: 'center', marginLeft: 10 }}>
+          <TouchableOpacity onPress={cancelEdit}>
+            <FontAwesomeIcon icon={faCircleXmark} color="#FF0000" size={20}/>
+          </TouchableOpacity>
+          <Text style={{ marginLeft: 5, color:'#FF0000' }}>Você está editando uma tarefa!</Text>
+        </View>
+      ) }
+
+      <View style={styles.containerTask}>
+        <TextInput 
+          style={styles.input}
+          placeholder="O que vai fazer hoje?"
+          value={newTask}
+          onChangeText={ (text) => setNewTask(text) }
+          ref={ inputRef }
+        />
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList 
+        data={tasks}
+        keyExtractor={(item) => item.key}
+        renderItem={ ({ item }) => (
+          <TaskList data={item} deleteItem={handleDelete} editItem={handleEdit}/>
+        )}
+      />
     </SafeAreaView>
   )
 }
@@ -22,5 +140,32 @@ const styles = StyleSheet.create({
     paddingTop: 25,
     paddingHorizontal: 10,
     backgroundColor: '#F2F6FC'
+  },
+  containerTask: {
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    marginTop: 10
+  },
+  input: {
+    flex: 1,
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#141414',
+    height: 45
+  },
+  buttonAdd: {
+    backgroundColor: '#141414',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 5,
+    paddingHorizontal: 14,
+    borderRadius: 5
+  }, buttonText: {
+    color: '#FFF',
+    fontSize: 22
   }
 })
